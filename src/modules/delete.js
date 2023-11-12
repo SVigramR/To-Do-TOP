6c7aa3b1bb93c14ba35046a41be278e827dd5de7
SVigramR <sabareeshvigram654@gmail.com>
2023-11-12 06:22:36 +0530

    Completed the default index refresher when task is deleted

--- src/modules/delete.js
+++ src/modules/delete.js
@@ -1,5 +1,5 @@
 import { inboxArray } from "../script"
-import { findTodayIndex } from "./date"
+import { findDateIndex, findTodayIndex, evaluateWeekDays, evaluateUpcomingdays, evaluatePendingDays } from "./date"
 
 function deleteFunction() {
     const deleteTask = document.querySelectorAll('.delete')
@@ -19,40 +19,61 @@ function refreshIndex() {
     const defaultProjectDiv = document.querySelectorAll('.task-list')
     const taskDiv = document.querySelectorAll('.task-div')
     defaultProjectDiv.forEach(defaultProject => {
+            let index;
             if (defaultProject.id === 'inbox') {
-                let index = 0;
+                index = 0;
                 taskDiv.forEach(task => {
                     task.setAttribute('id', index)
                     index++          
                 });
             } else if (defaultProject.id === 'today') {
-                let index = 0;
+                index = 0;
                 let findTodayIndexFunc = findTodayIndex(inboxArray, defaultProject.id)
-                let today = () => {
+                const today = () => {
                     return findTodayIndexFunc[index]
                 }
                 taskDiv.forEach(task => {
                     task.setAttribute('id', today())
                     index++;
                 });
-            } // else if (defaultProject.id === 'week') {
-            //     for (let index = 0; index < filteredWeek.length; index++) {
-            //         for (let jIndex = 0; jIndex < weekDay.length; jIndex++) {
-            //         }
-            //     }
-            // } else if (defaultProject.id === 'upcoming') {
-            //     for (let index = 0; index < sortedUpcoming.length; index++) {
-            //         for (let jIndex = 0; jIndex < filteredUpcoming.length; jIndex++) {                       
-            //         }
-            //     }
-            // } else if (defaultProject.id === 'pending') {
-            //     for (let index = 0; index < sortedPending.length; index++) {
-            //         for (let jIndex = 0; jIndex < filteredPending.length; jIndex++) {
-            //         }
-            //     }
-            // } else if (defaultProject.id === 'completed') {
+            } else if (defaultProject.id === 'week') {
+                index = 0;
+                const filteredWeek = evaluateWeekDays(new Date())
+                const findWeekIndexFunc = findDateIndex(inboxArray, filteredWeek)
+                let weekIndex = findWeekIndexFunc.filter(array => array.length > 0).flat()
+                const week = () => {
+                    return weekIndex[index]
+                }
+                taskDiv.forEach(task => {
+                    task.setAttribute('id', week())
+                    index++;
+                });
+            } else if (defaultProject.id === 'upcoming') {
+                index = 0;
+                const sortedUpcoming = evaluateUpcomingdays(new Date(), inboxArray)
+                const findUpcomingIndexFunc = findDateIndex(inboxArray, sortedUpcoming)
+                let upcomingIndex = findUpcomingIndexFunc.flat()
+                const upcoming = () => {
+                    return upcomingIndex[index]
+                }
+                taskDiv.forEach(task => {
+                    task.setAttribute('id', upcoming())
+                    index++;
+                });
+            } else if (defaultProject.id === 'pending') {
+                index = 0;
+                const sortedPending = evaluatePendingDays(new Date(), inboxArray)
+                const findPendingIndexFunc = findDateIndex(inboxArray, sortedPending)
+                let pendingIndex = findPendingIndexFunc.flat()
+                const upcoming = () => {
+                    return pendingIndex[index]
+                }
+                taskDiv.forEach(task => {
+                    task.setAttribute('id', upcoming())
+                    index++;
+                });
+            } // else if (defaultProject.id === 'completed') {
             // }
-        // });
     }); 
 }
 
